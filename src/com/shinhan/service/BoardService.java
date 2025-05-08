b880d458391722070826af91bc7e32df57742c2b
Chaewon257 <codnjs257@naver.com>
2025-05-08 11:20:36 +0900

    modify: boardService(add update)

--- src/com/shinhan/service/BoardService.java
+++ src/com/shinhan/service/BoardService.java
@@ -2,12 +2,18 @@ package com.shinhan.service;
 
 import com.shinhan.DTO.BoardDTO;
 import com.shinhan.dao.InsertDAO;
+import com.shinhan.dao.UpdateDAO;
 
 public class BoardService {
 
 	InsertDAO insertDAO = new InsertDAO();
+	UpdateDAO updateDAO = new UpdateDAO();
 
 	public int memberInsert(BoardDTO board) {
 		return insertDAO.memberInsert(board);
+	}
+	
+	public int MemberUpdate(BoardDTO board) {
+		return updateDAO.MemberUpdate(board);
 	}
 }
