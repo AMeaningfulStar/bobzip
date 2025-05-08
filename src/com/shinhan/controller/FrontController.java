0f26b175cf872d0e363765764c4030e0a0dc1ff4
Chaewon257 <codnjs257@naver.com>
2025-05-08 09:42:03 +0900

    add: controller

---
+++ src/com/shinhan/controller/FrontController.java
@@ -0,0 +1,11 @@
+package com.shinhan.controller;
+
+public class FrontController implements CommonControllerInterface{
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
