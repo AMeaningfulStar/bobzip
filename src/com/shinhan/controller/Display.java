5eb115de2693ee2076a2aee2f461dc079dfa270c
A Meaningful star <ghrlfehd123@naver.com>
2025-05-08 10:10:00 +0900

    edit: Service start display

---
+++ src/com/shinhan/controller/Display.java
@@ -0,0 +1,18 @@
+package com.shinhan.controller;
+
+public class Display {
+	public static void displayStart() {
+		System.out.println("==================================");
+		System.out.println("      BobZip 맛집 리뷰 게시판     ");
+		System.out.println("==================================");
+		System.out.println("1. 로그인");
+		System.out.println("2. 회원가입");
+		System.out.println("3. 프로그램 종료");
+		System.out.println("==================================");
+		System.out.print("원하시는 메뉴 번호를 선택하세요: ");
+	}
+	
+	
+	
+	
+}
