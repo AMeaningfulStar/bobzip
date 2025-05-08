c8cde7edaf22b74dd13a5ba9d7838d7051913876
Chaewon257 <codnjs257@naver.com>
2025-05-08 10:34:04 +0900

    add: insert

--- src/com/shinhan/service/BoardService.java
+++ src/com/shinhan/service/BoardService.java
@@ -1,5 +1,13 @@
 package com.shinhan.service;
 
+import com.shinhan.DTO.BoardDTO;
+import com.shinhan.dao.InsertDAO;
+
 public class BoardService {
 
+	InsertDAO insertDAO = new InsertDAO();
+
+	public int memberInsert(BoardDTO board) {
+		return insertDAO.memberInsert(board);
+	}
 }
