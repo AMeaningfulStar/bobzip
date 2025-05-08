ee882bde871c398dc3d27a047e8a214e6df6521b
Chaewon257 <codnjs257@naver.com>
2025-05-08 09:39:21 +0900

    add: util

--- src/com/shinhan/util/DButil.java
+++ src/com/shinhan/util/DButil.java
@@ -1,5 +1,37 @@
 package com.shinhan.util;
 
-public class DButil {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class DButil {
+	// DB 연결함수
+		public static Connection getConnection() {
+			Connection conn = null;
+//			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String url = "jdbc:oracle:thin:@192.168.0.240:1521:xe";
+			String userid = "hr", userpass = "hr";
+			try { 
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				conn = DriverManager.getConnection(url, userid, userpass);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			return conn;
+		}
+		// DB 연결시 사용한 자원 해제하는 함수
+		public static void dbDisconnect(Connection conn, Statement st, ResultSet rs) {
+			try {
+				if(rs != null) rs.close();
+				if(st != null) st.close();
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
 }
