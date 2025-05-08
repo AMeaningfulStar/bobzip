f8be86e4e927c580cb6cc1f0acfd902f50601d2f
Chaewon257 <codnjs257@naver.com>
2025-05-08 09:51:39 +0900

    add: BoardDTO

--- src/com/shinhan/DTO/BoardDTO.java
+++ src/com/shinhan/DTO/BoardDTO.java
@@ -1,5 +1,12 @@
 package com.shinhan.DTO;
 
+import java.sql.Date;
+
 public class BoardDTO {
-	 
+	 private String board_id;
+	 private String title;
+	 private String cate;
+	 private String contents;
+	 private Date wdate;
+	 private String pid; 
 }
