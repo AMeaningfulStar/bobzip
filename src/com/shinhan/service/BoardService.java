357ce768c511a385ad294b1e73ac93fbba1b98a4
yoonjunglv <yoonjung450@gmail.com>
2025-05-08 11:54:15 +0900

    add: boardService

--- src/com/shinhan/service/BoardService.java
+++ src/com/shinhan/service/BoardService.java
@@ -2,12 +2,14 @@ package com.shinhan.service;
 
 import java.util.List;
 
+import com.shinhan.dao.BoarddeleteDAO;
 import com.shinhan.dao.BoardselectDAO;
 import com.shinhan.dto.BoardDTO;
 
 public class BoardService {
 	
 	BoardselectDAO boardselectDAO = new BoardselectDAO();
+	BoarddeleteDAO boarddeleteDAO = new BoarddeleteDAO();
 	
 	public List<BoardDTO> selectALL(){
 		return boardselectDAO.selectALL();
@@ -15,6 +17,10 @@ public class BoardService {
 
 	public BoardDTO selectById(String board_id) {
 		return boardselectDAO.selectById(board_id);
+	}
+	
+	public int boardDeleteById(String board_id) {
+		return boarddeleteDAO.boardDeleteById(board_id);
 	}
 
 }
