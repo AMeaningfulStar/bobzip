6c752b2881af602e318f4b7a52e011106c8e204b
A Meaningful star <ghrlfehd123@naver.com>
2025-05-08 10:20:25 +0900

    edit: front controller - start flow

--- src/com/shinhan/controller/FrontController.java
+++ src/com/shinhan/controller/FrontController.java
@@ -1,11 +1,49 @@
 package com.shinhan.controller;
 
-public class FrontController implements CommonControllerInterface{
+import java.util.Scanner;
 
-	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-		
+public class FrontController {
+	static Scanner input = new Scanner(System.in);
+	static boolean user = false;
+
+	public static void main(String[] args) {
+		boolean isServiceOff = false;
+
+		while (!isServiceOff) {
+			user = startServiceFlow();
+
+			if (user == false) {
+				break;
+			} else {
+			}
+		}
+
+		input.close();
+		Display.displayExit();
+	}
+
+	private static boolean startServiceFlow() {
+		while (true) {
+			Display.displayStart();
+			int startInput = input.nextInt();
+			input.nextLine();
+
+			switch (startInput) {
+			case 1 -> {
+				return true;
+			}
+			case 2 -> {
+				return true;
+			}
+			case 3 -> {
+				return false;
+			}
+			default -> {
+				Display.displayError("메뉴에 있는 번호 중 하나를 선택해주세요.");
+				continue;
+			}
+			}
+		}
 	}
 
 }
